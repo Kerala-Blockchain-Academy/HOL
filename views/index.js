a1cbadc351951073334a62032de17fd229fff087
dlakhan <lekhan1991@yahoo.co.in>
2018-12-23 21:42:41 +0530

    Updated naming convention on html pages

--- views/index.js
+++ views/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var path = require('path');
-    //var {SimpleWalletClient} = require('./SimpleWalletClient') 
+// var {ProjectHolClient} = require('./ProjectHolClient') 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -10,39 +10,26 @@ router.get('/', function(req, res){
     res.redirect("/manufacturer");
 })
 
-//Get home view
+//Get Manufacturer view
 router.get('/manufacturer', function(req, res){
     res.sendFile(path.join(__dirname, '/clients/manufacturer.html'));
 });
 
-//Get main view
+//Get POS view
 router.get('/sale', function(req, res){
     res.sendFile(path.join(__dirname, '/clients/sale.html'));
 });
 
-// Get Deposit view
+// Get Stockist view
 router.get('/stockist',function(req, res){
     res.sendFile(path.join(__dirname, '/clients/stockist_to_warehouse.html'));
 })
 
-//Get Withdraw view
+//Get Warehouse view
 router.get('/warehouse',function(req, res){
     res.sendFile(path.join(__dirname, '/clients/warehouse_to_pos.html'));
 })
 
-/*
-
-//Get Transfer View
-router.get('/transfer',function(req, res){
-    res.render('transferPage');
-})
-
-//Get Balance View
-router.get('/balance', function(req, res){
-    res.render('balancePage');
-})
-
-*/
 
 
 module.exports = router;
